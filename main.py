2761f01db5cbdb4d67b4e561943ea87560a767eb
Tim <rrrraaaaaap@gmail.com>
2024-09-17 18:26:10 +0300

    add new code, Stores plan

--- main.py
+++ main.py
@@ -1,50 +1,79 @@
-from datetime import date
+# Ты разрабатываешь программное обеспечение для сети магазинов. Каждый магазин в этой сети имеет свои особенности, но также существуют общие характеристики, такие как адрес, название и ассортимент товаров. Ваша задача — создать класс Store, который можно будет использовать для создания различных магазинов.
+# Шаги:
+# 1. Создай класс Store:
+# -Атрибуты класса:
+# name: название магазина.
+# address: адрес магазина.
+# items: словарь, где ключ - название товара, а значение - его цена. Например, {'apples': 0.5, 'bananas': 0.75}.
+# Методы класса:
+# __init__ - конструктор, который инициализирует название и адрес, а также пустой словарь дляitems`.
+# -  метод для добавления товара в ассортимент.
+# метод для удаления товара из ассортимента
+# метод для получения цены товара по его названию. Если товар отсутствует, возвращайте None.
+# метод для обновления цены товара.
+# 2. Создай несколько объектов класса Store:
+# Создай не менее трех различных магазинов с разными названиями, адресами и добавь в каждый из них несколько товаров.
+# 3. Протестировать методы:
+# Выбери один из созданных магазинов и протестируй все его методы: добавь товар, обнови цену, убери товар и запрашивай цену.
+# В поле для ответа загрузи ссылку на GitHub-репозиторий, содержащий код проекта с реализацией задания.
 
+class Store():
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.items = {}
 
-class Task:
-    def __init__(self, description, due_date, status):
-        self.description = description
-        self.due_date = due_date
-        self.status = status
+    def add_item(self, item, price):
+        self.items[item] = price
 
+    def remove_item(self, item):
+        if item in self.items:
+            del self.items[item]
 
-    def get_description(self):
-        return self.description
+    def get_price(self, item):
+        return self.items.get(item, None)
 
-    def get_due_date(self):
-        return self.due_date
+    def update_price(self, item, new_price):
+        if item in self.items:
+            self.items[item] = new_price
 
-    def get_status(self):
-        if self.status == 'Выполнено':
-            return 'Задача уже выполнена.'
-        elif self.status == 'Не выполнено':
-            return 'Задача не выполнена'
+store1 = Store('FindWear', ' Batumi,Arganekidze 18')
+store2 = Store('Cropper', 'Manaco, Raite No ma note 34/35')
+store3 = Store('Victoria', 'Kutaisi, Kutaisi 23')
 
+store1.add_item('Shoes', 150)
+store1.add_item('Socks', 75)
+store1.add_item('Shirt', 100)
+store1.add_item('Pants', 125)
 
-task1 = Task('Организация хранения', f'{1}-сентябрь', 'Выполнено')
-task2 = Task('Сделать домашнее задание', f'{date(2024, 9, 1)}', 'Не выполнено')
-task3 = Task('Чистка кухни', f'{date(2024, 9, 1)}', 'Не выполнено')
-task4 = Task('Садовые работы', f'{date(2024, 9, 1)}', 'Выполнено')
+store2.add_item('Shoes', 175)
+store2.add_item('Socks', 100)
+store2.add_item('Shirt', 150)
+store2.add_item('Pants', 150)
 
+store3.add_item('Shoes', 125)
+store3.add_item('Socks', 120)
+store3.add_item('Shirt', 120)
+store3.add_item('Pants', 175)
 
-print(task1.get_description())
-print(task1.get_due_date())
-print(task1.get_status())
+print(store1.get_price('Shoes'))  # Output: 150
+store1.update_price('Shoes', 160)
+print(store1.get_price('Shoes'))  # Output: 160
+store1.remove_item('Socks')
+print(store1.get_price('Socks'))  # Output: None
 
-print(task2.get_description())
-print(task2.get_due_date())
-print(task2.get_status())
+print(store2.get_price('Shirt'))  # Output: 150
+store2.update_price('Shirt', 130)
+print(store2.get_price('Shirt'))  # Output: 130
+store2.remove_item('Shirt')
+print(store2.get_price('Shirt'))
 
-print(task3.get_description())
-print(task3.get_due_date())
-print(task3.get_status())
-
-print(task4.get_description())
-print(task4.get_due_date())
-print(task4.get_status())
-
-
-
+print(store3.get_price('Pants'))  # Output: 175
+store3.update_price('Pants', 200)
+print(store3.get_price('Pants'))  # Output: 200
+store3.remove_item('Shoes')
+print(store3.get_price('Shoes'))
+# Output: None
 
 
 
